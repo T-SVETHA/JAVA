6dadd7d85f5f3b5b6530ecd288d1539b6e072a25
T-SVETHA <154828084+T-SVETHA@users.noreply.github.com>
2024-07-11 22:57:21 +0530

    .

--- Assignment/A2.java
+++ Assignment/A2.java
@@ -191,12 +191,10 @@ public class RightTriangleStarPattern {
         System.out.print("Enter the number of rows: ");
         int n = scanner.nextInt();
         
-        for (int i = 1; i <= n; i++) {
-            for (int j = i; j < n; j++) {
-                System.out.print(" ");
-            }
-            for (int k = 1; k <= i; k++) {
-                System.out.print("* ");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <=i; j++) {
+                System.out.print("*");
+            
             }
             System.out.println();
         }
